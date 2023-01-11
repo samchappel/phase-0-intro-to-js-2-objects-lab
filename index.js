2901861c47809d8398cad937e284900ebf613f57
Sam Chappel <m.samchappel@gmail.com>
2023-01-11 10:20:22 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "14641 450th AVE SE North Bend, WA 98045",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee, key}
+    delete newEmployee.name;
+    delete newEmployee.streetAddress;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+}
